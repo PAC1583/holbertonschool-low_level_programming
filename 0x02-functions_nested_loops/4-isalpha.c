9c79ffa844cbdf321ab1f5a52eed7d886d2a821c
root <root@LAPTOP-67TDADFM.localdomain>
2022-02-15 09:14:34 -0500

    nested_loops

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * print_alphabet - determine is the char is letter or not
+ * _isalpha - determine if the char is a letter or not
  * @c: a character
  *
  * Return: 1 if true .
@@ -13,14 +13,14 @@ int _isalpha(int c)
 {
 if  (c < 97 || c > 123)
 {
-    if (c < 65 || c > 90)
-    {
-    return (0);
-    }
-    else
-    {
-    return (1);
-    }
+if (c < 65 || c > 90)
+{
+return (0);
+}
+else
+{
+return (1);
+}
 }
 else
 {
