ddfa0a3c89721ad02ae14251a927aa58f094af01
root <root@LAPTOP-67TDADFM.localdomain>
2022-02-15 08:49:29 -0500

    nested_loops

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,11 +4,17 @@
 /**
  * print_alphabet_x10 - prints the  10x alphabet to stdout
  * @: no variables
- * print_alphabet - prints the alphabet to stdout
- * @: no variables
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
 */
+
+/**
+ * print_alphabet - prints the alphabet to stdout
+ * @: no variables
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
 
 void print_alphabet(void)
 {
