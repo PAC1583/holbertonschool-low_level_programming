0c27b20651e4f3ca875ced33d548014c42ac2002
root <root@LAPTOP-67TDADFM.localdomain>
2022-02-17 12:33:13 -0500

    more nested loops

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,7 +10,7 @@ void more_numbers(void)
 {
 	int i, j;
 	int k = 0;
-	int num[10]={ 48, 49, 50, 51, 52, 53, 54, 55, 56, 57};
+	int num[10] = { 48, 49, 50, 51, 52, 53, 54, 55, 56, 57};
 
 	for (i = 0; i < 10; i++)
 	{
@@ -19,16 +19,16 @@ void more_numbers(void)
 			_putchar(num[k]);
 			k += 1;
 			if (j > 8)
-				{	
-					if (j == 9)
-					{
-						k = 0;
-					}
-					if (j != 14)
-					{
-						_putchar(num[1]);
-					}
+			{
+				if (j == 9)
+				{
+					k = 0;
 				}
+				if (j != 14)
+				{
+					_putchar(num[1]);
+				}
+			}
 		}
 		k = 0;
 		_putchar('\n');
