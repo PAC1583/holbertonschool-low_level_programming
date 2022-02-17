358cc038cca9b9831879862ebe6fc6ac59fcec6e
root <root@LAPTOP-67TDADFM.localdomain>
2022-02-17 11:26:17 -0500

    more nested functions

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * print_numbers - prints the numbers, from 0 to 9, followed by a new line.
+ * more_numbers - prints the numbers, from 0 to 14, followed by a new line.
  * @: none
  * Return: none
  */
