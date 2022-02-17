429a4f51cc24e9b00446ebf9e370c087b5f08100
root <root@LAPTOP-67TDADFM.localdomain>
2022-02-17 12:01:36 -0500

    more nested functions

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,24 +1,6 @@
 #include "main.h"
 
 /**
- * print_numbers - prints the numbers, from 0 to 9, followed by a new line.
- * @: none
- * Return: none
- */
-
-void print_numbers(void)
-{
-	int i;
-
-	for (i = 48; i < 58; i++)
-	{
-		_putchar(i);
-	};
-}
-
-
-
-/**
  * more_numbers - prints x10 the numbers, from 0 to 14, followed by a new line.
  * @: none
  * Return: none
@@ -28,9 +10,13 @@ void more_numbers(void)
 {
 	int i, j;
 
-	for (i = 0; i < 10; i++)
+	for (i = 0; i < 15; i++)
 	{
-		print_numbers();
+
+		for (j = 48; j < 58; j++)
+		{
+			_putchar(j);
+		}
 		for (j = 48; j < 53; j++)
 		{
 			_putchar(49);
