1e034fb84c93ddd1df4ff3300bc1861ef9df398e
root <root@LAPTOP-67TDADFM.localdomain>
2022-02-17 10:48:18 -0500

    debugging

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -13,9 +13,9 @@ int main(void)
 
 	i = 0;
 
-	while (i < 10)
+	while (i < 10) /*i is not changing. it is always 0*/
 	{
-		putchar(i); #i is not changing. it is always 0
+		putchar(i);
 	}
 
 	printf("Infinite loop avoided! \\o/\n");
