ee05402ce7c7d46439f849e0cda9c923a8cccb94
root <root@LAPTOP-67TDADFM.localdomain>
2022-02-17 22:25:11 -0500

    debugging

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -14,21 +14,21 @@ void print_remaining_days(int month, int day, int year)
 {
 	if (year % 4 == 0)
 	{
-		if ((year % 100 != 0) || ((year % 400 == 0) && (year % 100 == 0)))
+		if ((year % 100 != 0) || (year % 400 == 0 && year % 100 == 0))
 		{
 			if (month >= 2 && day >= 60)
 			{
 				day++;
 			}
 
-			printf("Day of the year: %d  ", day);
-			printf("Remaining days: %d  ", 366 - day);
-			printf("leap\n");
+			printf("Day of the year: %d\n", day);
+			printf("Remaining days: %d\n", 366 - day);
 		}
 		else
 		{
 			printf("Day of the year: %d\n", day);
 			printf("Remaining days: %d\n", 365 - day);
+
 		}
 	}
 	else
@@ -44,3 +44,4 @@ void print_remaining_days(int month, int day, int year)
 		}
 	}
 }
+
