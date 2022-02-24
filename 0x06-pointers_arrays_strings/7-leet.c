695318d72f6d392599e15fef30a413445744a99d
root <root@LAPTOP-67TDADFM.localdomain>
2022-02-24 16:30:45 -0500

    more pointers

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -9,22 +9,22 @@
  */
 char *leet(char *s)
 {
-    int i, n, j;
-    int letters[6] = {97, 101, 108, 111, 116};
-    int nums[6] = {52, 51, 49, 48, 55};
+	int i, n, j;
+	int letters[6] = {97, 101, 108, 111, 116};
+	int nums[6] = {52, 51, 49, 48, 55};
 
-    i = 0;
-    while (s[i] != '\0')
-    {
-        n = s[i];
-        for (j = 0; j < 5; j++)
-        {
-            if ((n == letters[j]) || (n == (letters[j] - 32)))
-            {
-                s[i] = nums[j];
-            }
-        }
-        i++;
-    }
-    return (s);
+	i = 0;
+	while (s[i] != '\0')
+	{
+		n = s[i];
+		for (j = 0; j < 5; j++)
+		{
+			if ((n == letters[j]) || (n == (letters[j] - 32)))
+			{
+				s[i] = nums[j];
+			}
+		}
+		i++;
+	}
+	return (s);
 }
