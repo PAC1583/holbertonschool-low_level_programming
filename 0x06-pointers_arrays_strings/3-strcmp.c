31ebeea239d66890f648b251ebbc7c9de6f33c0b
root <root@LAPTOP-67TDADFM.localdomain>
2022-02-24 11:19:05 -0500

    more pointers

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,18 +10,18 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i, j, n;
+	int i, j;
 
-	i = 0, j = 0;
+	i = 0;
 	while (s1[i] != '\0')
 	{
-		if (s1[i] != s2[j])
+		j = s1[i] - s2[i];
+
+		if ((j > 0) || (j < 0))
 		{
-			n = s1[0] - s2[0];
-			return (n);
+			return (j);
 		}
 		i++;
-		j++;
 	}
 	return (0);
 }
