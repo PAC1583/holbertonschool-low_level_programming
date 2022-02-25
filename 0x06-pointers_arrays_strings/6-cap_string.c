e4e8baab890f5944939a51237334a52fa2304565
root <root@LAPTOP-67TDADFM.localdomain>
2022-02-25 09:57:01 -0500

    more pointers

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,7 +9,7 @@
  */
 char *cap_string(char *s)
 {
-	int i, n, m, j;
+	int i, n, m, j, l;
 	int signs[12] = {32, 33, 46, 44, 59, 63, 34, 40, 41, 123, 125};
 
 	i = 0;
@@ -18,12 +18,14 @@ char *cap_string(char *s)
 		m = s[i];
 		for (j = 0; j < 10; j++)
 		{
-			if (m == signs[j] || (s[i] == '\n') || (s[i] == '\t'))
+			if (m == signs[j] || (s[i] == '\n') || (s[i] == '\t') || s[i] == '\"' || (i == 0))
 			{
-				if (s[i + 1] > 96)
+				l = (i != 0) ? 1 : 0;
+
+				if ((s[i + l] > 96))
 				{
-					n = s[i + 1] - 97;
-					s[i + 1] = 65 + n;
+					n = s[i + l] - 97;
+					s[i + l] = 65 + n;
 				}
 			}
 		}
