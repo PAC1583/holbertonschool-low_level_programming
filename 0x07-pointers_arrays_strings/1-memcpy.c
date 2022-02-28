33933cb9065cc208dfd9d48a6b05bde91f650d2b
root <root@LAPTOP-67TDADFM.localdomain>
2022-02-28 18:51:35 -0500

    more and more pointers, arrays and strings

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * *_memset -    function that fills memory with a constant byte.
+ * *_memcpy -    function that fills memory with a constant byte.
  *
  * @dest: address of memory of destination array
  * @src: address of memory of source array
@@ -11,14 +11,14 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-    unsigned int i;
+	unsigned int i;
 
-    i = 0;
-    while (i < n)
-    {
-        dest[i] = src[i];
-        i++;
-    }
+	i = 0;
+	while (i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
 
-    return (dest);
+	return (dest);
 }
