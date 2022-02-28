296cd6aed864912d562dde65e56023c0a40c2194
root <root@LAPTOP-67TDADFM.localdomain>
2022-02-28 18:33:12 -0500

    more and more pointers, arrays and strinfs

---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,6 @@
+#More pointers, arrays and strings
+
+*pointer to pointers
+*arrays 2D
+*multidimensional array 
+
---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+/**
+ *main - calls the function _putchar
+ *Return: Always 0
+ */
+int _putchar(char c);
+char *_memset(char *s, char b, unsigned int n);
