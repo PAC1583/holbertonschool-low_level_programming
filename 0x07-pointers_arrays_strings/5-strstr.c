104910fc3c5b6234cd7dc6faf162f5389d7354d5
root <root@LAPTOP-67TDADFM.localdomain>
2022-02-28 21:53:17 -0500

    more and more pointers, arrays and strings

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * *_strstr -  function that locates a substring.
+ *
+ * @haystack: a pointer to a string
+ * @needle: a pointer to a string
+ * Return: a char
+ */
+char *_strstr(char *haystack, char *needle)
+{
+    unsigned int i, j;
+
+    i = 0;
+    while (haystack[i] != '\0')
+    {
+        if (needle[0] == haystack[i])
+        {
+            j = 1;
+            while (needle[j] != '\0')
+            {
+                if (needle[j] != haystack[i + j])
+                {
+                    break;
+                }
+                j++;
+            }
+            return (&haystack[i]);
+        }
+        i++;
+    }
+    return (NULL);
+}
