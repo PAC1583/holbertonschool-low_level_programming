bd90a958d1d7aa1d33d0d5d33d9be3d8a1b04a1f
root <root@LAPTOP-67TDADFM.localdomain>
2022-02-28 23:27:52 -0500

    more and more pointers, arrays and strings

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -9,18 +9,18 @@
  */
 void print_chessboard(char (*a)[8])
 {
-    int i, j;
+	int i, j;
 
-    i = 0;
-    while (a[i][8] != '\0')
-    {
-        j = 0;
-        while (j < 8)
-        {
-            _putchar(a[i][j]);
-            j++;
-        }
-        _putchar('\n');
-        i++;
-    }
+	i = 0;
+	while (a[i][8] != '\0')
+	{
+		j = 0;
+		while (j < 8)
+		{
+			_putchar(a[i][j]);
+			j++;
+		}
+		_putchar('\n');
+		i++;
+	}
 }
