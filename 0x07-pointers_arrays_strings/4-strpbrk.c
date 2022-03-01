7a78f71c64e51b95a9a8dfdb22a016a1f586b192
root <root@LAPTOP-67TDADFM.localdomain>
2022-02-28 21:27:51 -0500

    more and more pointers, arrays and strings

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -10,21 +10,21 @@
  */
 char *_strpbrk(char *s, char *accept)
 {
-    unsigned int i, j;
+	unsigned int i, j;
 
-    i = 0;
-    while (s[i] != '\0')
-    {
-        j = 0;
-        while (accept[j] != '\0')
-        {
-            if (accept[j] == s[i])
-            {
-                return (&s[i]);
-            }
-            j++;
-        }
-        i++;
-    }
-    return (NULL);
+	i = 0;
+	while (s[i] != '\0')
+	{
+		j = 0;
+		while (accept[j] != '\0')
+		{
+			if (accept[j] == s[i])
+			{
+				return (&s[i]);
+			}
+			j++;
+		}
+		i++;
+	}
+	return (NULL);
 }
